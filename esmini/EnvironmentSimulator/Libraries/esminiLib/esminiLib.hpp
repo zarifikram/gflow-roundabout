f31dad5bd2efe7c3f0ebf1e6545032401776fab8
zarifikram <65703522+zarifikram@users.noreply.github.com>
2023-06-20 17:31:33 +0600

    edited

---
+++ esmini/EnvironmentSimulator/Libraries/esminiLib/esminiLib.hpp
@@ -0,0 +1,468 @@
+﻿/*
+ * esmini - Environment Simulator Minimalistic
+ * https://github.com/esmini/esmini
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) partners of Simulation Scenarios
+ * https://sites.google.com/view/simulationscenarios
+ */
+
+#pragma once
+
+#ifdef WIN32
+	#define SE_DLL_API __declspec(dllexport)
+#else
+	#define SE_DLL_API  // Leave empty on Mac
+#endif
+
+#define SE_PARAM_NAME_SIZE 32
+
+
+typedef struct
+{
+	int id;					  // Automatically generated unique object id
+	int model_id;             // Id to control what 3D model to represent the vehicle - see carModelsFiles_[] in scenarioenginedll.cpp
+	int ctrl_type;            // 0: DefaultController 1: External. Further values see Controller::Type enum
+	float timestamp;
+	float x;
+	float y;
+	float z;
+	float h;
+	float p;
+	float r;
+	int roadId;
+	float t;
+	int laneId;
+	float laneOffset;
+	float s;
+	float speed;
+	float centerOffsetX;
+	float centerOffsetY;
+	float centerOffsetZ;
+	float width;
+	float length;
+	float height;
+} SE_ScenarioObjectState;
+
+typedef struct
+{
+	float global_pos_x;     // target position, in global coordinate system
+	float global_pos_y;     // target position, in global coordinate system
+	float global_pos_z;     // target position, in global coordinate system
+	float local_pos_x;      // target position, relative vehicle (pivot position object) coordinate system
+	float local_pos_y;      // target position, relative vehicle (pivot position object) coordinate system
+	float local_pos_z;      // target position, relative vehicle (pivot position object) coordinate system
+	float angle;			// heading angle to target from and relatove to vehicle (pivot position)
+	float road_heading;		// road heading at steering target point
+	float road_pitch;		// road pitch (inclination) at steering target point
+	float road_roll;		// road roll (camber) at target point
+	float trail_heading;	// trail heading (only when used for trail lookups, else equals road_heading)
+	float curvature;		// road curvature at steering target point
+	float speed_limit;		// speed limit given by OpenDRIVE type entry
+} SE_RoadInfo;
+
+typedef struct
+{
+	int far_left_lb_id;
+	int left_lb_id;
+	int right_lb_id;
+	int far_right_lb_id;
+} SE_LaneBoundaryId;
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+	float h;
+	float p;
+	float speed;
+} SE_SimpleVehicleState;
+
+typedef struct
+{
+	const char* name;  // Name of the parameter as defined in the OpenSCENARIO file
+	void* value;  // Pointer to value which can be an integer, double or string (const char*) as defined in the OpenSCENARIO file
+} SE_Parameter;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	// Basic interface
+	// 
+	
+	/**
+	Add a search path for OpenDRIVE and 3D model files
+	@param path Path to a directory
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_AddPath(const char* path);
+
+	/**
+	Clear all search paths for OpenDRIVE and 3D model files
+	*/
+	SE_DLL_API void SE_ClearPaths();
+
+	/**
+	Initialize the scenario engine
+	@param oscFilename Path to the OpenSCEANRIO file
+	@param disable_ctrls 1=Any controller will be disabled 0=Controllers applied according to OSC file
+	@param use_viewer 0=no viewer, 1=use viewer
+	@param threads 0=single thread, 1=viewer in a separate thread, parallel to scenario engine
+	@param record Create recording for later playback 0=no recording 1=recording
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_Init(const char *oscFilename, int disable_ctrls, int use_viewer, int threads, int record);
+
+	/**
+	Initialize the scenario engine
+	@param oscAsXMLString OpenSCENARIO XML as string
+	@param disable_ctrls 1=Any controller will be disabled 0=Controllers applied according to OSC file
+	@param use_viewer 0=no viewer, 1=use viewer
+	@param threads 0=single thread, 1=viewer in a separate thread, parallel to scenario engine
+	@param record Create recording for later playback 0=no recording 1=recording
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_InitWithString(const char* oscAsXMLString, int disable_ctrls, int use_viewer, int threads, int record);
+
+	/**
+	Initialize the scenario engine
+	@param oscFilename Path to the OpenSCEANRIO file
+	@param argc Number of arguments
+	@param argv Arguments
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_InitWithArgs(int argc, char* argv[]);
+
+	/**
+	Step the simulation forward with specified timestep
+	@param dt time step in seconds
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_StepDT(float dt);
+
+	/**
+	Step the simulation forward. Time step will be elapsed system (world) time since last step. Useful for interactive/realtime use cases.
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_Step();
+
+	/**
+	Stop simulation gracefully. Two purposes: 1. Release memory and 2. Prepare for next simulation, e.g. reset object lists.
+	*/
+	SE_DLL_API void SE_Close();
+
+	/**
+	Get simulation time in seconds
+	*/
+	SE_DLL_API float SE_GetSimulationTime();  // Get simulation time in seconds
+
+	/**
+	Get the bool value of the end of the scenario
+	*/
+	SE_DLL_API int SE_GetQuitFlag();
+
+	/**
+	Get name of currently referred and loaded OpenDRIVE file 
+	@return filename as string (const, since it's allocated and handled by esmini)
+	*/
+	SE_DLL_API const char* SE_GetODRFilename();
+
+	/**
+	Get name of currently referred and loaded SceneGraph file 
+	@return filename as string (const, since it's allocated and handled by esmini)
+	*/
+	SE_DLL_API const char* SE_GetSceneGraphFilename();
+
+	/**
+	Set value of named parameter
+	@param parameter Struct object including name of parameter and pointer to value, see SE_Parameter declaration
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_SetParameter(SE_Parameter parameter);
+
+	/**
+	Get value of named parameter. The value within the parameter struct will be filled in.
+	@param parameter Pointer to parameter struct object, see SE_Parameter declaration.
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetParameter(SE_Parameter* parameter);
+
+	SE_DLL_API void* SE_GetODRManager();
+
+
+	/**
+	Report object position in cartesian coordinates
+	@param id Id of the object
+	@param timestamp Timestamp (not really used yet, OK to set 0)
+	@param x X coordinate
+	@param y Y coordinate
+	@param z Z coordinate
+	@param h Heading / yaw
+	@param p Pitch
+	@param r Roll
+	@param speed Speed in forward direction of the enitity
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_ReportObjectPos(int id, float timestamp, float x, float y, float z, float h, float p, float r, float speed);
+
+	/**
+	Report object position in road coordinates
+	@param id Id of the object
+	@param timestamp Timestamp (not really used yet, OK to set 0)
+	@param roadId Id of the road 
+	@param laneId Id of the lane 
+	@param laneOffset Lateral offset from center of specified lane
+	@param s Longitudinal distance of the position along the specified road
+	@param speed Speed in forward direction (s axis) of the enitity
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_ReportObjectRoadPos(int id, float timestamp, int roadId, int laneId, float laneOffset, float s, float speed);
+
+	/**
+	Report object longitudinal speed. Useful for an external longitudinal controller.
+	@param id Id of the object
+	@param speed Speed in forward direction of the enitity
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_ReportObjectSpeed(int id, float speed);
+
+	/**
+	Report object lateral position relative road centerline. Useful for an external lateral controller.
+	@param id Id of the object
+	@param t Lateral position
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_ReportObjectLateralPosition(int id, float t);
+
+	/**
+	Report object lateral position by lane id and lane offset. Useful for an external lateral controller.
+	@param id Id of the object
+	@param laneId Id of the lane
+	@param laneOffset Lateral offset from center of specified lane
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_ReportObjectLateralLanePosition(int id, int laneId, float laneOffset);
+
+	/**
+	Get the number of entities in the current scenario
+	@return Number of entities
+	*/
+	SE_DLL_API int SE_GetNumberOfObjects();
+
+	/**
+	Get the state of specified object
+	@param index Index of the object. Note: not ID
+	@param state Pointer/reference to a SE_ScenarioObjectState struct to be filled in
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetObjectState(int index, SE_ScenarioObjectState* state);
+
+	/**
+	Get the name of specified object
+	@param index Index of the object. Note: not ID
+	@return Name
+	*/
+	SE_DLL_API const char* SE_GetObjectName(int index);
+
+	/**
+	Check whether an object has a ghost (special purpose lead vehicle) 
+	@param index Index of the object. Note: not ID
+	@return 1 if ghost, 0 if not
+	*/
+	SE_DLL_API int SE_ObjectHasGhost(int index);
+
+	/**
+	Get the state of specified object's ghost (special purpose lead vehicle)
+	@param index Index of the object. Note: not ID
+	@param state Pointer/reference to a SE_ScenarioObjectState struct to be filled in
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetObjectGhostState(int index, SE_ScenarioObjectState* state);
+
+	/**
+	Get information suitable for driver modeling of a point at a specified distance from object along the road ahead
+	@param object_id Handle to the position object from which to measure
+	@param lookahead_distance The distance, along the road, to the point
+	@param data Struct including all result values, see typedef for details
+	@param lookAheadMode Measurement strategy: Along 0=lane center, 1=road center (ref line) or 2=current lane offset. See roadmanager::Position::LookAheadMode enum
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetRoadInfoAtDistance(int object_id, float lookahead_distance, SE_RoadInfo* data, int lookAheadMode);
+
+	/**
+	Get information suitable for driver modeling of a ghost vehicle driving ahead of the ego vehicle
+	@param object_id Handle to the position object from which to measure (the actual externally controlled Ego vehicle, not ghost)
+	@param lookahead_distance The distance, along the ghost trail, to the point from the current Ego vehicle location
+	@param data Struct including all result values, see typedef for details
+	@param speed_ghost reference to a variable returning the speed that the ghost had at this point along trail
+	@return 0 if successful, -1 if not
+	*/
+	SE_DLL_API int SE_GetRoadInfoAlongGhostTrail(int object_id, float lookahead_distance, SE_RoadInfo* data, float* speed_ghost);
+
+	/**
+	Create an ideal object sensor and attach to specified vehicle
+	@param object_id Handle to the object to which the sensor should be attached
+	@param x Position x coordinate of the sensor in vehicle local coordinates
+	@param y Position y coordinate of the sensor in vehicle local coordinates
+	@param z Position z coordinate of the sensor in vehicle local coordinates
+	@param h heading of the sensor in vehicle local coordinates
+	@param fovH Horizontal field of view, in degrees
+	@param rangeNear Near value of the sensor depth range
+	@param rangeFar Far value of the sensor depth range
+	@param maxObj Maximum number of objects theat the sensor can track
+	@return Sensor ID (Global index of sensor), -1 if unsucessful
+	*/
+	SE_DLL_API int SE_AddObjectSensor(int object_id, float x, float y, float z, float h, float rangeNear, float rangeFar, float fovH, int maxObj);
+
+	/**
+	Fetch list of identified objects from a sensor
+	@param sensor_id Handle (index) to the sensor
+	@param list Array of object indices
+	@return Number of identified objects, i.e. length of list. -1 if unsuccesful.
+	*/
+	SE_DLL_API int SE_FetchSensorObjectList(int sensor_id, int* list);
+
+	/**
+	Register a function and optional parameter (ref) to be called back from esmini after each frame (update of scenario)
+	The current state of specified entity will be returned.
+	Complete or part of the state can then be overridden by calling the SE_ReportObjectPos/SE_ReportObjectRoadPos functions.
+	@param object_id Handle to the position object (entity)
+	@param SE_ScenarioObjectState A pointer to the function to be invoked
+	@param user_data Optional pointer to a local data object that will be passed as argument in the callback. Set 0/NULL if not needed.
+	*/
+	SE_DLL_API void SE_RegisterObjectCallback(int object_id, void (*fnPtr)(SE_ScenarioObjectState*, void*), void* user_data);
+
+
+	// OSI interface
+	// 
+
+	/**
+	Send OSI packages over UDP to specified IP address
+	*/
+	SE_DLL_API int SE_OpenOSISocket(const char *ipaddr);
+
+	/**
+	Switch off logging to OSI file(s)
+	*/
+	SE_DLL_API void SE_DisableOSIFile();
+
+	/**
+	Switch on logging to OSI file(s)
+	@param filename Optional filename, including path. Set to 0 or "" to use default.
+	*/
+	SE_DLL_API void SE_EnableOSIFile(const char* filename);
+
+	/**
+	The SE_UpdateOSIGroundTruth function returns a char array containing the osi GroundTruth serialized to a string
+	*/
+	SE_DLL_API int SE_UpdateOSIGroundTruth();
+
+	/**
+	The SE_GetOSIGroundTruth function returns a char array containing the osi GroundTruth serialized to a string
+	*/
+	SE_DLL_API const char* SE_GetOSIGroundTruth(int* size);
+
+	/**
+	The SE_GetOSIGroundTruthRaw function returns a char array containing the OSI GroundTruth information
+	@return osi3::GroundTruth*
+	*/
+	SE_DLL_API const char* SE_GetOSIGroundTruthRaw();
+
+	/**
+	The SE_GetOSIRoadLane function returns a char array containing the osi Lane information/message of the lane where the object with object_id is, serialized to a string
+	*/
+	SE_DLL_API const char* SE_GetOSIRoadLane(int* size, int object_id);
+	
+	/**
+	The SE_GetOSIRoadLane function returns a char array containing the osi Lane Boundary information/message with the specified GLOBAL id
+	*/
+	SE_DLL_API const char* SE_GetOSILaneBoundary(int* size, int global_id);
+	
+	/**
+	The SE_GetOSILaneBoundaryIds function the global ids for left, far elft, right and far right lane boundaries
+	@param object_id Handle to the object to which the sensor should be attached
+	@param ids Reference to a struct which will be filled with the Ids
+	*/
+	SE_DLL_API void SE_GetOSILaneBoundaryIds(int object_id, SE_LaneBoundaryId* ids);
+
+	/**
+	The SE_GetOSISensorDataRaw function returns a char array containing the OSI SensorData information
+	@return osi3::SensorData*
+	*/
+	SE_DLL_API const char* SE_GetOSISensorDataRaw();
+
+	/**
+	Create and open osi file
+	*/
+	SE_DLL_API bool SE_OSIFileOpen(const char* filename);
+
+	/**
+	Create and open osi file
+	*/
+	SE_DLL_API bool SE_OSIFileWrite(bool flush = false);
+
+	SE_DLL_API void SE_LogMessage(char* message);
+
+	// Viewer settings
+	/**
+	The SE_GetOSILaneBoundaryIds function the global ids for left, far elft, right and far right lane boundaries
+	@param featureType Type of the features, see viewer::NodeMask typedef
+	@param enable Set true to show features, false to hide
+	*/
+	SE_DLL_API void SE_ViewerShowFeature(int featureType, bool enable);
+
+	// Simple vehicle 
+	/**
+	Create an instance of a simplistic vehicle based on a 2D bicycle kincematic model
+	@param x Initial position X world coordinate
+	@param y Initial position Y world coordinate
+	@param h Initial heading
+	@param length Length of the vehicle
+	@return Handle to the created object
+	*/
+	SE_DLL_API void* SE_SimpleVehicleCreate(float x, float y, float h, float length);
+
+	/**
+	Delete an instance of the simplistic vehicle model
+	*/
+	SE_DLL_API void SE_SimpleVehicleDelete(void* handleSimpleVehicle);
+
+	/**
+	Control the speed and steering with discreet [-1, 0, 1] values, suitable for keyboard control (e.g. up/none/down).
+	The function also steps the vehicle model, updating its position according to motion state and timestep.
+	@param dt timesStep (s)
+	@param throttle Longitudinal control, -1: brake, 0: none, +1: accelerate
+	@param steering Lateral control, -1: left, 0: straight, 1: right
+	*/
+	SE_DLL_API void SE_SimpleVehicleControlBinary(void* handleSimpleVehicle, double dt, int throttle, int steering);  // throttle and steering [-1, 0 or 1]
+
+	/**
+	Control the speed and steering with floaing values in the range [-1, 1], suitable for driver models.
+	The function also steps the vehicle model, updating its position according to motion state and timestep.
+	@param dt timesStep (s)
+	@param throttle Longitudinal control, -1: maximum brake, 0: no acceleration, +1: maximum acceleration
+	@param steering Lateral control, -1: max left, 0: straight, 1: max right
+	*/
+	SE_DLL_API void SE_SimpleVehicleControlAnalog(void* handleSimpleVehicle, double dt, double throttle, double steering);  // throttle and steering [-1, 0 or 1]
+
+	/**
+	Set maximum vehicle speed.
+	@param speed Maximum speed (m/s)
+	*/
+	SE_DLL_API void SE_SimpleVehicleSetMaxSpeed(void* handleSimpleVehicle, float speed);
+
+	/**
+	Get current state of the vehicle. Typically called after Control has been applied.
+	@param state Pointer/reference to a SE_SimpleVehicleState struct to be filled in
+	*/
+	SE_DLL_API void SE_SimpleVehicleGetState(void* handleSimpleVehicle, SE_SimpleVehicleState* state);
+	
+#ifdef __cplusplus
+}
+#endif
---
+++ esmini/EnvironmentSimulator/Libraries/esminiRMLib/esminiRMLib.hpp
@@ -0,0 +1,236 @@
+/*
+ * esmini - Environment Simulator Minimalistic
+ * https://github.com/esmini/esmini
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) partners of Simulation Scenarios
+ * https://sites.google.com/view/simulationscenarios
+ */
+
+#pragma once
+
+#ifdef WIN32
+  #define RM_DLL_API __declspec(dllexport)
+#else
+  #define RM_DLL_API  // Leave empty on Mac
+#endif
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+	float h;
+	float p;
+	float r;
+	float hRelative;
+	int   roadId;
+	int   laneId;
+	float laneOffset;
+	float s;
+} RM_PositionData;
+
+typedef struct
+{
+	float pos[3];			// position, in global coordinate system
+	float heading;			// road heading 
+	float pitch;			// road pitch 
+	float roll;				// road roll
+	float width;
+	float curvature;
+	float speed_limit;
+} RM_RoadLaneInfo;
+
+typedef struct
+{
+	RM_RoadLaneInfo road_lane_info; // Road info at probe location
+	float relative_pos[3];          // probe position, relative vehicle (pivot position object) coordinate system
+	float relative_h;		        // heading angle to steering target from and relatove to vehicle (pivot position)
+} RM_RoadProbeInfo;
+
+typedef struct
+{
+	float ds;				// delta s (longitudinal distance)
+	float dt;				// delta t (lateral distance)
+	int dLaneId;			// delta laneId (increasing left and decreasing to the right)
+} RM_PositionDiff;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	RM_DLL_API int RM_Init(const char *odrFilename);
+
+	RM_DLL_API int RM_Close();
+
+	/**
+	Create a position object
+	@return Handle to the position object, to use for operations
+	*/
+	RM_DLL_API int RM_CreatePosition();
+
+	/**
+	Get the number of created position objects
+	@return Number of created position objects
+	*/
+	RM_DLL_API int RM_GetNrOfPositions();
+
+	/**
+	Delete one or all position object(s)
+	@param hande Handle to the position object. Set -1 to delete all.
+	@return 0 if succesful, -1 if specified position(s) could not be deleted
+	*/
+	RM_DLL_API int RM_DeletePosition(int handle);
+
+	/**
+	Get the total number fo roads in the road network of the currently loaded OpenDRIVE file.
+	@return Number of roads
+	*/
+	RM_DLL_API int RM_GetNumberOfRoads();
+
+	/**
+	Get the Road ID of the road with specified index. E.g. if there are 4 roads, index 3 means the last one.
+	@param index The index of the road
+	@return The ID of the road
+	*/
+	RM_DLL_API int RM_GetIdOfRoadFromIndex(int index);
+
+	/**
+	Get the lenght of road with specified ID
+	@param id The road ID 
+	@return The length of the road if ID exists, else 0.0
+	*/
+	RM_DLL_API float RM_GetRoadLength(int id);
+
+	/**
+	Get the number of drivable lanes of specified road
+	@param roadId The road ID
+	@param s The distance along the road at what point to check number of lanes (which can vary along the road)
+	@return The number of drivable lanes
+	*/
+	RM_DLL_API int RM_GetRoadNumberOfLanes(int roadId, float s);
+
+	/**
+	Get the ID of the lane given by index
+	@param roadId The road ID
+	@param laneIndex The index of the lane 
+	@param s The distance along the road at what point to look up the lane ID
+	@return The lane ID
+	*/
+	RM_DLL_API int RM_GetLaneIdByIndex(int roadId, int laneIndex, float s);
+
+	/**
+	Set position from road coordinates, world coordinates being calculated
+	@param handle Handle to the position object
+	@param roadId Road specifier
+	@param laneId Lane specifier
+	@param laneOffset Offset from lane center
+	@param s Distance along the specified road
+	@param align If true the heading will be reset to the lane driving direction (typically only at initialization)
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetLanePosition(int handle, int roadId, int laneId, float laneOffset, float s, bool align);
+
+	/**
+	Set s (distance) part of a lane position, world coordinates being calculated
+	@param handle Handle to the position object
+	@param s Distance along the specified road
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetS(int handle, float s);
+
+	/**
+	Set position from world coordinates, road coordinates being calculated
+	@param handle Handle to the position object
+	@param x cartesian coordinate x value
+	@param y cartesian coordinate y value
+	@param z cartesian coordinate z value
+	@param h rotation heading value
+	@param p rotation pitch value
+	@param r rotation roll value
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetWorldPosition(int handle, float x, float y, float z, float h, float p, float r);
+
+	/**
+	Set position from world X, Y and heading coordinates; Z, pitch and road coordinates being calculated
+	@param handle Handle to the position object
+	@param x cartesian coordinate x value
+	@param y cartesian coordinate y value
+	@param h rotation heading value
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetWorldXYHPosition(int handle, float x, float y, float h);
+
+	/**
+	Set position from world X, Y, Z and heading coordinates; pitch and road coordinates being calculated
+	Setting a Z value may have effect in mapping the position to the closest road, e.g. overpass
+	@param handle Handle to the position object
+	@param x cartesian coordinate x value
+	@param y cartesian coordinate y value
+	@param h rotation heading value
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_SetWorldXYHPosition(int handle, float x, float y, float h);
+
+	/**
+	Move position forward along the road. Choose way randomly though any junctions.
+	@param handle Handle to the position object
+	@param dist Distance (meter) to move
+	@param strategy How to move in a junction where multiple route options appear, see Junction::JunctionStrategyType
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_PositionMoveForward(int handle, float dist, int strategy);
+
+	/**
+	Get the fields of the position of specified index
+	@param handle Handle to the position object
+	@param data Struct to fill in the values
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetPositionData(int handle, RM_PositionData *data);
+
+	/**
+	Retrieve current speed limit (at current road, s-value and lane) based on ODR type elements or nr of lanes
+	@param handle Handle to the position object
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API float RM_GetSpeedLimit(int handle);
+
+	/**
+	Retrieve lane information from the position object (at current road, s-value and lane)
+	@param handle Handle to the position object
+	@param lookahead_distance The distance, along the road, to the point of interest
+	@param data Struct including all result values, see RM_RoadLaneInfo typedef
+	@param lookAheadMode Measurement strategy: Along reference lane, lane center or current lane offset. See roadmanager::Position::LookAheadMode enum
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetLaneInfo(int handle, float lookahead_distance, RM_RoadLaneInfo *data, int lookAheadMode);
+
+	/**
+	As RM_GetLaneInfo plus relative location of point of interest (probe) from current position
+	@param handle Handle to the position object from which to measure
+	@param lookahead_distance The distance, along the road to the probe (point of interest)
+	@param data Struct including all result values, see RM_RoadProbeInfo typedef
+	@param lookAheadMode Measurement strategy: Along reference lane, lane center or current lane offset. See roadmanager::Position::LookAheadMode enum
+	@return 0 if successful, -1 if not
+	*/
+	RM_DLL_API int RM_GetProbeInfo(int handle, float lookahead_distance, RM_RoadProbeInfo *data, int lookAheadMode);
+
+	/**
+	Find out the difference between two position objects, i.e. delta distance (long and lat) and delta laneId
+	@param handleA Handle to the position object from which to measure
+	@param handleB Handle to the position object to which the distance is measured
+	@param pos_diff Struct including all result values, see PositionDiff typedef
+	@return true if a valid path between the road positions was found and calculations could be performed
+	*/
+	RM_DLL_API bool RM_SubtractAFromB(int handleA, int handleB, RM_PositionDiff *pos_diff);
+
+
+#ifdef __cplusplus
+}
+#endif
